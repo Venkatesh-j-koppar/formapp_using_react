19902103bde872641ef49cd288c8a67bb8265fd9
Venkatesh <venkateshkoppar@gmail.com>
2022-10-31 20:30:52 +0530

    Version1

--- src/App.js
+++ src/App.js
@@ -17,19 +17,24 @@ function App() {
       type: "text",
       name: "username",
       label: "username",
+      errormessage:
+        "Username should be 3-16 character and shouldnot contain special charater",
       placeholder: "Username",
+      pattern: "^[A-Za-z0-9]{3,16}$",
     },
     {
       id: 2,
       type: "email",
       name: "email",
+      errormessage: "Please enter valid email",
       label: "Email",
       placeholder: "Email",
     },
     {
       id: 3,
-      type: "text",
+      type: "date",
       name: "birthday",
+      errormessage: "please enter birth day",
       label: "Birthday",
       placeholder: "Birthday",
     },
@@ -37,15 +42,19 @@ function App() {
       id: 4,
       type: "password",
       name: "password",
+      errormessage: "please enter valid password",
       label: "password",
       placeholder: "password",
+      pattern: "^(?=.*[A-Za-z])(?=.*d)(?=.*[@$!%*#?&])[A-Za-zd@$!%*#?&]{8,}$",
     },
     {
       id: 5,
       type: "password",
       name: "confirmpassword",
+      errormessage: " password didnot match",
       label: "confirm password",
       placeholder: "confirm password",
+      pattern: values.password,
     },
   ];
 
@@ -62,14 +71,17 @@ function App() {
   return (
     <div className="app">
       <form onSubmit={handleSubmit}>
+        <h1>Register</h1>
         {inputs.map((input) => (
           <FormInput
             key={input.id}
             label={input.label}
             placeholder={input.placeholder}
             name={input.name}
+            errormessage={input.errormessage}
             value={values[input.name]}
             onChange={onChange}
+            {...input}
           ></FormInput>
         ))}
 
--- src/Components/FormInput.js
+++ src/Components/FormInput.js
@@ -1,11 +1,23 @@
+import { useState } from "react";
 import "./formInput.css";
 const FormInput = (props) => {
-  const { label, onChange, id, ...inputProps } = props;
+  const { label, onChange, id, errormessage, ...inputProps } = props;
+  const [focused, setFocused] = useState(false);
 
+  const handlefocus = () => {
+    setFocused(true);
+  };
   return (
     <div className="formInput">
       <label>{label}</label>
-      <input {...inputProps} onChange={onChange} />
+      <input
+        {...inputProps}
+        onChange={onChange}
+        required={true}
+        onBlur={handlefocus}
+        focused={focused.toString()}
+      />
+      <span>{errormessage}</span>
     </div>
   );
 };
